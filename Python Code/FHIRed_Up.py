275aca446449172d400598c151db3d1ee24cd575
Spiro Ganas <spiroganas@gmail.com>
2016-02-19 17:20:24 -0500

    more refactoring

--- Python Code/FHIRed_Up.py
+++ Python Code/FHIRed_Up.py
@@ -21,23 +21,35 @@ def GetHCCsByTimePeriod(PatientID, CurrentYear):
     for Enc in Encounters:
         # loop over all the contitions in the encounter
         for Cond in Enc[2]:
-
-
-            # TODO: for testing purposes, I'm using snomed code.
-            # I need to get the conversion software working correctly
-            ICD9 = (Cond[0], Cond[1])
-            #ICD9 = SNOWMED_to_HCC(Cond[0])
+            # One of our assumptions was that the GaTech FHIR server stored
+            # diagnosis data using ICD9 codes.  This assumption was wrong.
+            # Since the FHIR server uses SNOMED codes to store clinical infomation
+            # we developed code an lookup tables to convert SNOMED codes to ICD9
+            # codes.  After extensive testing, we were unable to develop code that
+            # could perform this very complicated mapping accuratly.
+            # So we decided to return SNOMED data to the end user, instead of
+            # returning ICD9s or HCCs.
+            #
+            # This is the code we would have used if we could have got the
+            # mapping to work.  Our problem was that a one-to-one mapping between
+            # these two coding systems simply doesn't exist.
+            #
+            # This is what the original solution would have been:
+            #DiagnosisCode = SNOWMED_to_HCC(Cond[0])
+            #
+            # This is the work-around code:
+            DiagnosisCode = (Cond[0], Cond[1])
 
 
             if Enc[1] == CurrentYear:
-                CurrentYearHCCs.append(ICD9)
+                CurrentYearHCCs.append(DiagnosisCode)
             elif Enc[1]<CurrentYear:
-                HistoricalHCCs.append(ICD9)
+                HistoricalHCCs.append(DiagnosisCode)
                 
 
     CurrentYearHCCs = set(CurrentYearHCCs)
     HistoricalHCCs = set(HistoricalHCCs)
-    return (CurrentYearHCCs, HistoricalHCCs)
+    return (list(CurrentYearHCCs), list(HistoricalHCCs))
 
 
 def Find_Missing_Diagnoses(CurrentYearHCCs, HistoricalHCCs):
@@ -72,6 +84,31 @@ if __name__ == "__main__":
 
     import pprint
 
+    PatientID = 4
+    CurrentYear = 2010
+
+
+    CurrentYearHCCs = GetHCCsByTimePeriod(PatientID, CurrentYear)[0]
+    HistoricalHCCs = GetHCCsByTimePeriod(PatientID, CurrentYear)[1]
+
+
+    print "Current Year HCCs:"
+    pprint.pprint(CurrentYearHCCs)
+    print ""
+    print "Historical Years HCCs:"
+    pprint.pprint(HistoricalHCCs)
+    print ""
+    print "Missing HCCs:"
+    pprint.pprint(Find_Missing_Diagnoses(CurrentYearHCCs, HistoricalHCCs))
+    print ""
+    print "Missing HCCs - by patient and year version:"
+    pprint.pprint(FindMissingDiagnosesByPatientID(PatientID, CurrentYear))    
+    print ""
+    print ""
+    print ""
+
+
+
     PatientID = 5
     CurrentYear = 2010
 
@@ -83,6 +120,31 @@ if __name__ == "__main__":
     print "Current Year HCCs:"
     pprint.pprint(CurrentYearHCCs)
     print ""
+    print "Historical Years HCCs:"
+    pprint.pprint(HistoricalHCCs)
+    print ""
+    print "Missing HCCs:"
+    pprint.pprint(Find_Missing_Diagnoses(CurrentYearHCCs, HistoricalHCCs))
+    print ""
+    print "Missing HCCs - by patient and year version:"
+    pprint.pprint(FindMissingDiagnosesByPatientID(PatientID, CurrentYear))    
+    print ""
+    print ""
+    print ""
+
+
+
+
+    PatientID = 6
+    CurrentYear = 2010
+
+
+    CurrentYearHCCs = GetHCCsByTimePeriod(PatientID, CurrentYear)[0]
+    HistoricalHCCs = GetHCCsByTimePeriod(PatientID, CurrentYear)[1]
+
+
+    print "Current Year HCCs:"
+    pprint.pprint(CurrentYearHCCs)
     print ""
     print "Historical Years HCCs:"
     pprint.pprint(HistoricalHCCs)
@@ -92,6 +154,10 @@ if __name__ == "__main__":
     print ""
     print "Missing HCCs - by patient and year version:"
     pprint.pprint(FindMissingDiagnosesByPatientID(PatientID, CurrentYear))    
+    print ""
+    print ""
+    print ""
+
 
 
 
