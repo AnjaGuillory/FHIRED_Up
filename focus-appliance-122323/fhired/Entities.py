9775df027c256b03655a37c3bd480e8b41c98fd8
Guillory, Anja Monique <anjag@gatech.edu>
2016-03-12 12:39:21 -0600

    Update Entities.py
    added a listOfDiag variable to hold the list of diagnoses for that particular Patient. Also added class Provider (for use of the doctor, nurse, etc.) which may contain a provider id or name, and their list of patients which would then be used in query for get_patient_for function

--- focus-appliance-122323/fhired/Entities.py
+++ focus-appliance-122323/fhired/Entities.py
@@ -1,9 +1,17 @@
 
 class Patient:
 
-    def __init__(self, pt_id, name, dob, gender, address):
+    def __init__(self, pt_id, name, dob, gender, address, listOfDiag):
         self.pt_id = pt_id
         self.name = name
         self.dob = dob
         self.gender = gender
         self.address = address
+        self.listOfDiag
+        
+    
+class Provider:
+    def __init__(self, prov_id, name, listOfPatients):
+        self.prov_id = prov_id
+        self.name = name
+        self.listOfPatients
