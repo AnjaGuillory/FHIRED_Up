18f97c914978515d941e34cd216107a140f9b56c
Daniel Stoneburner <dfstoneburner@gmail.com>
2016-04-09 15:03:40 -0500

    Added converter tests

---
+++ focus-appliance-122323/tests/converter_tests.py
@@ -0,0 +1,25 @@
+import unittest
+
+from fhired.SnowmedConverter import SnowmedConverter
+
+
+class ConverterTests(unittest.TestCase):
+    def setUp(self):
+        self.converter = SnowmedConverter()
+
+    def test_convert_snowmed_to_hcc(self):
+        hcc = self.converter.to_hcc('151004')
+
+        self.assertIsNotNone(hcc)
+        self.assertEquals('3', hcc[0])
+        self.assertEquals('Central Nervous System Infection', hcc[1])
+
+    def test_n_hcc_convert_snowmed_to_hcc(self):
+        hcc = self.converter.to_hcc('147001')
+
+        self.assertIsNone(hcc)
+
+    def test_fail_convert_snowmed_to_hcc(self):
+        hcc = self.converter.to_hcc('000000')
+
+        self.assertIsNone(hcc)
